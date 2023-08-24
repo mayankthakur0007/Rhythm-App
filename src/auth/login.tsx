048df3031e6c9f2c518fecd56b44b38f88a07a07
mayankthakur0007 <mayankthakur231@gmail.com>
2023-08-24 11:05:54 +0530

    login screen

--- src/auth/login.tsx
+++ src/auth/login.tsx
@@ -1,48 +1,83 @@
 import { useState } from 'react';
-import { KeyboardAvoidingView, Platform, TextInput, TouchableOpacity, View } from 'react-native';
+import { Image, KeyboardAvoidingView, Platform, TextInput, TouchableOpacity, View } from 'react-native';
 import RTText from '../custom/rcctext';
 
 export default function Login() {
-  const [number, onChangeNumber] = useState('');
+  const [eId, onChangeEId] = useState('');
+  const [password, onChangePassword] = useState('');
 
   return (
-    <KeyboardAvoidingView style={{ flex: 1 }} behavior={Platform.OS === 'ios' ? 'padding' : undefined}>
-      <View style={{ flex: 6, paddingVertical: '6%', alignItems: 'center' }}>
+    <KeyboardAvoidingView
+      style={{flex: 1}}
+      behavior={Platform.OS === 'ios' ? 'padding' : undefined}>
+      <View style={{flex: 6, paddingVertical: '6%', alignItems: 'center'}}>
+        <Image
+          style={{
+            width: '50%',
+            height: '50%',
+            resizeMode: 'contain',
+          }}
+          source={require('../assets/logo.png')}
+        />
         <RTText
           weight="semiBold"
           style={{
             fontSize: 20,
             lineHeight: 24,
-            marginBottom: 57,
+            marginBottom: 17,
             textAlign: 'center',
           }}>
-          LOGIN OR SIGNUP
         </RTText>
-        <View style={{ alignItems: 'center', width: '70%' }}>
+        <View style={{alignItems: 'center', width: '70%'}}>
           <RTText
             weight="regular"
             style={{
-              paddingBottom: 6,
+              paddingVertical: 6,
               fontSize: 14,
               lineHeight: 17,
               alignSelf: 'flex-start',
             }}>
-            Enter Mobile Number
+            Enter Employee ID
           </RTText>
           <TextInput
-            style={[{
-              borderWidth: 1,
-              width: '100%',
-              color: "black",
-              paddingVertical: 12,
-              paddingHorizontal: 20,
-            }]}
-            onChangeText={onChangeNumber}
-            inputMode="tel"
+            style={[
+              {
+                borderWidth: 1,
+                width: '100%',
+                color: 'black',
+                paddingVertical: 12,
+                paddingHorizontal: 20,
+              },
+            ]}
+            onChangeText={onChangeEId}
+            inputMode="text"
             maxLength={10}
-            value={number}></TextInput>
-          <TouchableOpacity
-            style={{ width: '100%' }}>
+            value={eId}></TextInput>
+          <RTText
+            weight="regular"
+            style={{
+              paddingVertical: 6,
+              fontSize: 14,
+              lineHeight: 17,
+              alignSelf: 'flex-start',
+            }}>
+            Enter Password
+          </RTText>
+          <TextInput
+            style={[
+              {
+                borderWidth: 1,
+                width: '100%',
+                color: 'black',
+                paddingVertical: 12,
+                paddingHorizontal: 20,
+              },
+            ]}
+            onChangeText={onChangePassword}
+            inputMode="text"
+            maxLength={10}
+            value={password}></TextInput>
+          <TouchableOpacity style={{width: '100%'}}>
             <RTText
               weight="medium"
               style={{
@@ -54,13 +89,12 @@ export default function Login() {
                 alignSelf: 'center',
                 textAlign: 'center',
               }}>
-              Continue
+              Login
             </RTText>
           </TouchableOpacity>
         </View>
       </View>
-      <View style={{ flex: 3, justifyContent: 'space-evenly' }}>
-      </View>
+      <View style={{flex: 3, justifyContent: 'space-evenly'}}></View>
     </KeyboardAvoidingView>
   );
 }
